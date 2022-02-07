1a45322c84b092521788e8774be20091a82fea36
Roberto Costa Tupinamba <robertotupinamba@gmail.com>
2022-02-07 01:02:15 -0300

    Tipando e acessando propriedades no Styled Components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -2,17 +2,30 @@ import React from 'react';
 
 import { Container, Header, Title, Icon, Footer, Amount, LastTransition } from './styles';
 
-export function HighlightCard() {
+interface Props {
+  type: 'up' | 'down' | 'total';
+  title: string;
+  amount: string;
+  lastTransition: string;
+}
+
+const icon = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+  total: 'dollar-sign',
+};
+
+export function HighlightCard({ type, title, amount, lastTransition }: Props) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entrada</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransition>Ultima entrada dia 13 de abril</LastTransition>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransition type={type}>{lastTransition}</LastTransition>
       </Footer>
     </Container>
   );
