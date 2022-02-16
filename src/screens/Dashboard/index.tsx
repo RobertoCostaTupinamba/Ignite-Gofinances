74f90dbf784da3817781e69bd897f68042a21e30
Roberto Costa Tupinamba <robertotupinamba@gmail.com>
2022-02-16 08:28:51 -0300

    Cartões de Transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
+
 import {
   Container,
   Header,
@@ -12,6 +14,8 @@ import {
   UserName,
   Icon,
   HighlightCards,
+  Transactions,
+  Title,
 } from './styles';
 
 export function Dashboard() {
@@ -41,6 +45,12 @@ export function Dashboard() {
         <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransition="Última saída dia 03 de abril" />
         <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransition="01 à 16 de abril" />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionCard />
+      </Transactions>
     </Container>
   );
 }
