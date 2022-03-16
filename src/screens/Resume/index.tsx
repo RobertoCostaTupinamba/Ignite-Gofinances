8eabf8db92f41efd8001793b888e2741b2182543
Roberto Costa Tupinamba <robertotupinamba@gmail.com>
2022-03-15 23:52:29 -0300

    Melhorando o carregamento do Gráfico

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -45,14 +45,13 @@ interface CategoryData {
 }
 
 export function Resume() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
 
   const theme = useTheme();
 
   function handleDateChange(action: 'next' | 'prev') {
-    setIsLoading(true);
     if (action === 'next') {
       setSelectedDate(addMonths(selectedDate, 1));
     } else {
@@ -61,6 +60,7 @@ export function Resume() {
   }
 
   async function loadData() {
+    setIsLoading(true);
     const dataKey = '@gofinances:transactions';
 
     const response = await AsyncStorage.getItem(dataKey);
@@ -111,14 +111,10 @@ export function Resume() {
     setIsLoading(false);
   }
 
-  useEffect(() => {
-    loadData();
-  }, [selectedDate]);
-
   useFocusEffect(
     useCallback(() => {
       loadData();
-    }, []),
+    }, [selectedDate]),
   );
 
   return (
@@ -133,7 +129,7 @@ export function Resume() {
       ) : (
         <Content
           showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ flex: 1, paddingHorizontal: 24, paddingBottom: 150 }}>
+          contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: useBottomTabBarHeight() }}>
           <MonthSelect>
             <MonthSelectButton onPress={() => handleDateChange('prev')}>
               <MonthSelectIcon name="chevron-left" />
